afb3443b27447329d2717f83e1883d1e78319671
Andy Li <andylizi666@gmail.com>
2024-12-30 12:41:09 -0400

    fix: 🐛 export `{from,to}Base64Url`

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ export * from './createToBase64Bin';
 export * from './createFromBase64';
 export * from './toBase64';
 export * from './toBase64Bin';
+export * from './toBase64Url';
 export * from './fromBase64';
 export * from './fromBase64Bin';
+export * from './fromBase64Url';
